96ae8dc411c791eb0830b0c110651f66e40f034f
VasaniDeepthi <67586096+VasaniDeepthi@users.noreply.github.com>
2020-07-05 16:52:37 +0530

    Add files via upload

---
+++ oneline.py
@@ -0,0 +1,13 @@
+import PyPDF2
+
+with open('oneline.pdf', 'rb') as pdf_file:
+    pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+
+    # printing first page contents
+    pdf_page = pdf_reader.getPage(0)
+    print(pdf_page.extractText())
+
+    # reading all the pages content one by one
+    for page_num in range(pdf_reader.numPages):
+        pdf_page = pdf_reader.getPage(page_num)
+        print(pdf_page.extractText())
